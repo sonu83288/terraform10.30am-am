5f37eaef97b3ef927394dcf6f71a995b59165d1a
sonu83288 <routchittadeep@gmail.com>
2025-11-22 18:17:16 +0530

    Add Jenkins agent instance and update resources

--- day-2-configuration-files/main.tf
+++ day-2-configuration-files/main.tf
@@ -1,11 +1,12 @@
-resource "aws_instance" "dev" {
-    ami = var.ami_id
-    instance_type = var.type
-    subnet_id = aws_subnet.name.id
-    tags = {
-      name ="dev"
-    }
+resource "aws_instance" "jenkins_agent" {
+  ami           = "ami-02b297871a94f4b42"  # Replace with a valid AMI ID
+  instance_type = "t3.micro"
   
+  tags = {
+    Name        = "Jenkins-Build-Agent-01"  # <--- THIS IS THE AWS INSTANCE NAME
+    Environment = "Dev"
+  }
+}
 }
 resource "aws_vpc" "name" {
     cidr_block = "10.0.0.0/24"
@@ -21,6 +22,7 @@ resource "aws_subnet" "name" {
   
 
 }
+
 
 
 
