744f461a7901ee3f52e32449b57787d401a36153
sonu83288 <routchittadeep@gmail.com>
2025-11-22 17:59:22 +0530

    Refactor AWS resources in main.tf

--- day-2-configuration-files/main.tf
+++ day-2-configuration-files/main.tf
@@ -7,14 +7,15 @@ resource "aws_instance" "dev" {
     }
   
 }
-resource "aws_vpc" "name" {
+resource "aws_vpc" "dev" {
     cidr_block = "10.0.0.0/24"
 
   
 }
-resource "aws_subnet" "name" {
+resource "aws_subnet" "devvv" {
     vpc_id = aws_vpc.name.id
     cidr_block = "10.0.0.0/24"
   
 
 }
+
